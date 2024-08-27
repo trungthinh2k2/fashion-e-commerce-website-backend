a4e5dd6480ead3a40a1c350896dbbe1ecff9f1ab
trungthinh2k2 <89030667+trungthinh2k2@users.noreply.github.com>
2024-08-27 09:31:28 +0700

    create project

---
+++ src/main/java/iuh/fit/fashionecommercewebsitebackend/FashionECommerceWebsiteBackendApplication.java
@@ -0,0 +1,13 @@
+package iuh.fit.fashionecommercewebsitebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FashionECommerceWebsiteBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FashionECommerceWebsiteBackendApplication.class, args);
+    }
+
+}
